75b98e7436f6e3010cb02836c7a01fa207ff2efc
Jimrich88 <james.alexander.richardson@gmail.com>
2016-01-20 16:36:19 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@
 print 'hello'
 urltoscrape='http://www.rockrun.com/'
 print urltoscrape
-myage=27
+print my age=27
 urltoscrape=urltoscrape+'p1'
 print urltoscrape
 # html = scraperwiki.scrape("http://foo.com")
